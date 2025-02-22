d284ea20e0510b7702e1f76f8622158b3f2f995a
Bello Qudus Folohunsho <qudusalabi19@gmail.com>
2025-02-22 23:34:36 +0100

    API key gotten

--- src/js/config.js
+++ src/js/config.js
@@ -1,5 +1,5 @@
 export const API_URL = 'https://forkify-api.jonas.io/api/v2/recipes/';
 export const TIMEOUT_SEC = 10;
 export const RES_PER_PAGE = 10;
-export const KEY = '<YOUR_KEY>';
+export const KEY = 'c03b6c08-aec5-4249-8f29-3bec4e5e23e3';
 export const MODAL_CLOSE_SEC = 2.5;
